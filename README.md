1c00e75d0eba89dccaba5246603006a79a0e6756
Penchev Valeri <val3ri.penchev@gmail.com>
2020-01-10 17:00:14 +0100

    tried some sortable tables; todo try sorttable.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Project for checking websites if they are online or offline
+
+to start: py hello.py
+
+// others
+to format: autopep8 --in-place --agressive hello.py (?!)
--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+from flask_table import Table, Col
+
 import logging
 
 import json
@@ -14,18 +16,42 @@ def hello():
 
     json_file = open("updated_db.json", "r")
     json_data = json.load(json_file)
-    # console.log(json_file)
+
+    print(json_data)
 
     # return str(json_data)
     return render_template('for-loop.html')
     # return """
     # Hello world!
     # This is my web app!
-    # """ + """ 
-    # """ + json_data[0]["url"] + """ 
+    # """ + """
+    # """ + json_data[0]["url"] + """
     # """
 
+# Declare your table
+class ItemTable(Table):
+    name = Col('Name')
+    description = Col('Description')
+
+# Or, equivalently, some dicts
+items = [dict(name='Name1', description='Description1'),
+         dict(name='Name2', description='Description2'),
+         dict(name='Name3', description='Description3')]
+
+# Populate the table
+table = ItemTable(items)
+
+
+@app.route("/hello")
+def hello1():
+    return render_template('home.html', table=table, name="valeri")
+
+@app.route("/test_table")
+def test_table():
+    return render_template('table.html')
 
 if __name__ == "__main__":
     # app.run()
     app.run(debug=True, use_reloader=True)
+
+
